0cb6806472e567f05133cc280d04d3188c2bcf83
Oleksandr Polotniuk <oleksandr.polotniuk@gmail.com>
2022-08-29 19:48:02 +0300

    EXPA ADD: Example of sign in form added

--- src/pages/Auth/SignIn/SignIn.tsx
+++ src/pages/Auth/SignIn/SignIn.tsx
@@ -2,24 +2,81 @@ import React from 'react';
 import './SignIn.css';
 import womanImage from '../../../assets/images/woman.png';
 import AuthContainer from '../../../components/AuthContainer/AuthContainer';
-import { Box } from '@mui/material';
+import { Avatar, Box, Button, Checkbox, FormControlLabel, Grid, Link, TextField, Typography } from '@mui/material';
 
 const SignIn: React.FC = () => {
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+    };
+
     return (
         <AuthContainer image={womanImage}>
-            <Box sx={{
-                position: 'absolute',
-                left: 150,
-                top: 200,
-                width: 400,
-                height: 400,
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                borderRadius: '50px',
-                backgroundColor: 'aqua',
-            }}>
-                Sign In Form
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    position: 'absolute',
+                    backgroundColor: 'lightblue',
+                    padding: '20px',
+                    boxSizing: 'content-box',
+                    borderRadius: '10px',
+                    width: '500px',
+                    left: 100,
+                    top: 150,
+                }}
+            >
+                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign in
+                </Typography>
+                <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                    />
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                    />
+                    <FormControlLabel
+                        control={<Checkbox value="remember" color="primary" />}
+                        label="Remember me"
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                    >
+                        Sign In
+                    </Button>
+                    <Grid container>
+                        <Grid item xs>
+                            <Link href="#" variant="body2">
+                                Forgot password?
+                            </Link>
+                        </Grid>
+                        <Grid item>
+                            <Link href="#" variant="body2">
+                                {"Don't have an account? Sign Up"}
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </Box>
             </Box>
         </AuthContainer>
     )
