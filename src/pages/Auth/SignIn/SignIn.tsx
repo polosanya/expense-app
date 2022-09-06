b5cc779cc346ec676e45f1ea43dafceef26f7511
Oleksandr Polotniuk <oleksandr.polotniuk@gmail.com>
2022-09-06 12:46:52 +0300

    EXPA UPD: custom error text added

--- src/pages/Auth/SignIn/SignIn.tsx
+++ src/pages/Auth/SignIn/SignIn.tsx
@@ -14,8 +14,12 @@ import * as Yup from "yup";
 
 export const SignIn: FC = () => {
   const validationSchema = Yup.object({
-    email: Yup.string().email().required(),
-    password: Yup.string().min(8, 'Password too short').required()
+    email: Yup.string()
+      .email('Your email is not valid')
+      .required('Please enter your email'),
+    password: Yup.string()
+      .min(8, 'Password too short. Minimum 8 symbols required.')
+      .required('Please enter your password')
   });
 
   const initialValues = {
